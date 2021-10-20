6b5c13718788b0bd81013fb8e4d9baecbc04f530
Jo <joanne.watson@manchester.ac.uk>
2021-10-20 17:09:08 +0100

    Organise and document data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Human gene list to be used as example input
+#'
+#' @format A data frame with 346 rows and 1 variables:
+#' \describe{
+#'   \item{v1}{Human genes}
+#'   ...
+#' }
+"Human_exampleGenes"
+
+#' Mouse gene list to be used as example input
+#'
+#' @format A data frame with 345 rows and 1 variables:
+#' \describe{
+#'   \item{v1}{Mouse genes}
+#'   ...
+#' }
+"Mouse_exampleGenes"
